e0e0c437183e73c09b2674753dd31a6c0945ee9b
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-11-26 13:11:40 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -9,11 +9,6 @@ const nextConfig = {
     GOOGLE_CLIENT_SECRET: "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",
     NEXTAUTH_SECRET:
       "7c985f1d478158c04ce8ac52e73b4a3e5223e0b7bf89996c57684d6fa288f912",
-    NEXTAUTH_URL:
-      "https://16742ba4-21fb-45f4-8e84-c7b52c6fb2f6-00-l80az4gsq7vk.kirk.replit.dev",
-    NEXT_PUBLIC_BASE_URL:
-      "https://16742ba4-21fb-45f4-8e84-c7b52c6fb2f6-00-l80az4gsq7vk.kirk.replit.dev",
-    NEXT_ENV: "development",
     NEXT_PUBLIC_POSTHOG_KEY:
       "phc_d5zPRbWhiVfhRs9xfjwhngZ86mIB4kPfhOfbcEGSrjc",
     NEXT_PUBLIC_POSTHOG_HOST: "https://us.i.posthog.com"
